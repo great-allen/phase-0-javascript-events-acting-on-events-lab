b10ce32394a21d63e09149761e84392df89db8e8
QunFan <allenfan1012@gmail.com>
2022-08-09 13:26:06 +1000

    commit tree 528db2d2f6f411a3295b8aceab96bcd46d6e3a7f

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+    const leftNumber = dodger.style.left.replace ("px", "");
+    const left = parseInt(leftNumber, 10);
+    if (left >0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+
+document.addEventListener('keydown', function(e){
+    if (e.key === 'Arrowleft'){
+        moveDodgerLeft();
+    }
+})
+
+function moveDodgerRight (){
+    const leftNumber = dodger.style.left.replace ("px", "");
+    const left = parseInt(leftNumber, 10);
+    if (left < 180) {
+        dodger.style.left =`${left +1}px`;
+    }
+}
+document.addEventListener('keydown', function(e){
+    if (e.key === 'Arrowleft'){
+        moveDodgerRight();
+    }
+})
